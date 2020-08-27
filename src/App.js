92a90977ea607e2659eec366ba0a69a2b4296cbc
mithunabinagar <mithunabinagar@gmail.com>
2020-08-28 01:12:27 +0600

    seven done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <div>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
